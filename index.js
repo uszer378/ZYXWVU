c7164e6ce4442b0b3d4080e8ae7b9e9ab0062acd
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2026-06-19 15:33:43 -0400

    0

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-class Person {
-    constructor(name, adult, age) {
-        this.name = name;
-        this.adult = adult;
-        this.age = age;
-        this.hobbies = hobbies;
-    }
-
-    set age(newAge) {
-        if (newAge < 14) {
-
-            alert(`${this.name} you can not get a job yet you have to wait until you are 14 or older`);
-        } else {
-            this._age = newAge;
-        }
-    }
-
-    get age() {
-        return this._age;
-    }
-}
+const rgb =  ['red', 'green',  'blue']
+const [r,g,b] = rgb
+alert(`${r}`)
+alert(`${g}`)
+alert(`${b}`)
