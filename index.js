0d422fbe23b52b514637ea180d98f36530355a96
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2026-07-14 10:19:59 -0400

    nice

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const rgb =  ['red', 'green',  'blue']
-const [r,g,b] = rgb
-alert(`${r}`)
-alert(`${g}`)
-alert(`${b}`)
+
+async function loadPersonData() {
+    let data = await fetch('person.json');
+    let jsoned = await data.json();
+    let name = document.getElementById('name')
+    let bday = document.getElementById('bday')
+    let job = document.getElementById('job')
+    let gender = document.getElementById('gender')
+    name.textContent = jsoned.name
+    job.textContent = jsoned.job
+    bday.textContent = jsoned.birthdate
+    gender.textContent = jsoned.gender
+    
+
+}
+loadPersonData();
