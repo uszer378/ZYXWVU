fb2f8c80a7942dd3387033ade4193f1c555bbe45
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2026-08-11 20:56:55 -0400

    maybe?

--- searcher/index.js
+++ searcher/index.js
@@ -1,7 +1,17 @@
-let input = document.getElementById('input').value.toLowerCase
+let input = document.getElementById('input').value.toLowerCase()
 let dis = document.getElementById('dis')
 async function getData() {
-  let response = await fetch(`../${input}/test.json`)
-  let son = await response.json()
-  dis.textContent = son.who
+  try{
+    let response = await fetch(`https://raw.githubusercontent.com/uszer378/ZYXWVU/refs/heads/main/${input}/test.json`)
+    if(!response.ok){
+      throw new Error("not seeing a thing");
+      dis.textContent = `not seeing a thing`
+      let son = await response.json()
+      dis.textContent = son.who
+    }
+  }
+  catch(error){
+    console.error()
+  }
+
 }
