80b0cbbb231a6cfe78c30850b948b158822e0519
uszer378 <mY_s0n_Sso0_dum-iq21@hotmail.com>
2026-08-13 20:08:55 -0400

    finallly

--- searcher/index.js
+++ searcher/index.js
@@ -1,17 +1,19 @@
-let input = document.getElementById('input').value.toLowerCase()
 let dis = document.getElementById('dis')
+let link = document.getElementById('link')
+let auth = document.getElementById('auth')
 async function getData() {
-  try{
+  const input = document.getElementById('input').value.toLowerCase()
+  try {
     let response = await fetch(`https://raw.githubusercontent.com/uszer378/ZYXWVU/refs/heads/main/${input}/test.json`)
-    if(!response.ok){
-      throw new Error("not seeing a thing");
-      dis.textContent = `not seeing a thing`
-      let son = await response.json()
-      dis.textContent = son.who
-    }
-  }
-  catch(error){
-    console.error()
-  }
 
+    if (!response.ok) {
+      dis.textContent = `not seeing a thing`
+      throw new Error('Network response was not ok')
+    }
+    let son = await response.json();
+    auth.textContent = son.who
+
+  } catch (error) {
+    
+  }
 }
